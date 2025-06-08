cc1361024ae8d944a1280be3f04fd1a3df6d396b
thanktoanf <thanhtoan0306@gmail.com>
2025-06-08 22:49:58 +0700

    add new game and adjust eslinit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,9 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {
+    ...globals.browser,
+    Phaser: "readonly",
+    Theodoric: "readonly" // Add this line to declare Theodoric as a global
+  } } },
 ]);
